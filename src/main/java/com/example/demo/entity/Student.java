46bfa19974f7f997944b20aa5123e695bff95507
pavan11 <pavansai3777@gmail.com>
2024-09-06 21:08:24 +0530

    first commit

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String studentName;
+	private String StudentEmail;
+	private String studentAdress;
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", studentName=" + studentName + ", StudentEmail=" + StudentEmail
+				+ ", studentAdress=" + studentAdress + "]";
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentEmail() {
+		return StudentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		StudentEmail = studentEmail;
+	}
+	public String getStudentAdress() {
+		return studentAdress;
+	}
+	public void setStudentAdress(String studentAdress) {
+		this.studentAdress = studentAdress;
+	}
+
+}
